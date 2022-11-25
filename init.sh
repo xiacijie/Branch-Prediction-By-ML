bde16ea3bf2330ead1ae753e0fd7d04e491a5356
Cijie Xia <cijie.xia@mail.utoronto.ca>
2022-11-25 13:49:44 -0500

    invoke python scripts in llvm

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
 export PROJECT_ROOT=$PWD 
 export LLVM=$PROJECT_ROOT/llvm-project/build/bin
+export MODEL_ROOT=$PROJECT_ROOT/models
--- models/model.py
+++ models/model.py
@@ -1,12 +1,1 @@
-import torch
-
-class MyModule(torch.nn.Module):
-    def __init__(self):
-        super(MyModule, self).__init__()
-
-    def forward(self):
-        return 998
-
-m = MyModule()
-sm = torch.jit.script(m)
-sm.save("./model.pt")
+print(123)
